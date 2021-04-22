60c75d6881fc9aa285d61b9a5be9e4e94e18bc22
katearzate <katearzate000@gmail.com>
2021-04-22 17:32:00 -0500

    Matriz relleno completa

--- TransposicionMatrices/transposicion.c
+++ TransposicionMatrices/transposicion.c
@@ -25,19 +25,43 @@ int main(){
 
 
 void matriz(char mensaje[MAX], int dim1){
-	int i = 0, dim2;
-	
-	while(mensaje[i]){
-		char caracter = mensaje[i];
-		if (!isalpha(caracter)){ //si no es letra
-			//mensaje[i]
-		}
-		i++;
-	}
-	dim2 = (i / dim1); //COLOCAR CONDICIONAL 
-	if((i%dim1) != 0){
+	int dim2, i, j;
+	size_t tamanoMen = 0;
+	//Definir tamaño de dim2 (renglones)
+	tamanoMen = strlen(mensaje);
+	dim2 = (tamanoMen / dim1); 
+	if((tamanoMen%dim1) != 0){
 		dim2+=1;
 	}
-	printf("caracteres: %d\n", i);
-	printf("la dimension: %d", dim2);
+	
+	//concatenar relleno del mensaje
+	int resto = (dim1 * dim2) - strlen(mensaje);
+	const char* relleno = "&";
+	for (i = 0; i < resto; i++)
+		strncat(mensaje, relleno, (strlen(mensaje)+i+1));
+	
+	//crear espacio de memoria para matriz
+	char** arreglo = (char**)malloc(sizeof(char) * dim2);
+	for (i = 0; i < dim1; i++)
+	{
+		arreglo[i] = (char*)malloc(sizeof(char) * dim1);
+		//strncpy(arreglo[i], msg, strlen(msg));
+	}
+	
+	//recorrer y llenar matriz
+	int posicion = 0;
+	for (i = 0; i < dim2; i++)
+	{
+		for (j = 0; j < dim1; j++)
+		{
+			if(mensaje[posicion] == ' ') 
+				mensaje[posicion] = '%';
+			arreglo[i][j] = mensaje[posicion];
+			printf("%c", arreglo[i][j]);
+			posicion++;
+		}
+		printf("\n");
+	}
+	
+	
 }
