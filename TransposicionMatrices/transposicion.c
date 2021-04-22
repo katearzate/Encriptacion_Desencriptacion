7bb6c8904f6cc2b2e091179752d20d6c023f0db9
katearzate <katearzate000@gmail.com>
2021-04-21 23:54:57 -0500

    Funcion para crear matriz

--- TransposicionMatrices/transposicion.c
+++ TransposicionMatrices/transposicion.c
@@ -4,6 +4,7 @@
 
 #define MAX 80
 
+void matriz(char mensaje[MAX], int dim1); 
 
 int main(){
 
@@ -14,21 +15,29 @@ int main(){
 	fgets(mensaje, MAX, stdin);
 	mensaje[strcspn(mensaje, "\r\n")] = 0;
 
-	int DIM;
+	int dim1;
 	printf("Ingresar dimension de columnas de matriz (3,4 ó 5)");
-	scanf("%d", &DIM);
+	scanf("%d", &dim1);
 	
-	int i = 0, tot;
-	while(mensaje[i]){
-		i++;
-	}
-	tot = (i / DIM); //COLOCAR CONDICIONAL 
-	if((i%DIM) != 0){
-		tot+=1;
-	}
-	printf("caracteres: %d\n", i);
-	printf("la dimension: %d", tot);
-	
+	matriz(mensaje, dim1);
 	return 0;
 }
 
+
+void matriz(char mensaje[MAX], int dim1){
+	int i = 0, dim2;
+	
+	while(mensaje[i]){
+		char caracter = mensaje[i];
+		if (!isalpha(caracter)){ //si no es letra
+			//mensaje[i]
+		}
+		i++;
+	}
+	dim2 = (i / dim1); //COLOCAR CONDICIONAL 
+	if((i%dim1) != 0){
+		dim2+=1;
+	}
+	printf("caracteres: %d\n", i);
+	printf("la dimension: %d", dim2);
+}
