f172b3d8a414929e874dc116068609c107b57797
katearzate <katearzate000@gmail.com>
2021-07-07 13:38:11 -0500

    chmod command working

--- linuxCommands/src/linuxcommands/LinuxCommands.java
+++ linuxCommands/src/linuxcommands/LinuxCommands.java
@@ -6,13 +6,13 @@
 package linuxcommands;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -26,11 +26,150 @@ public class LinuxCommands {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Scanner read = new Scanner(System.in);
         
+        String command = "7-- /home/katearzate/Desktop/ej";
+        
+        String[] datos = command.split(" ");
+        File file = new File(datos[1]);
+        
+        Set<PosixFilePermission> perms = new HashSet<>();
+        
+        for (int i = 0; i < 3; i++) {
+            switch(datos[0].charAt(i)){
+                case '-':
+                    System.out.println("no hay!");
+                    break;
+                case '1':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_EXECUTE);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_EXECUTE);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                            break;
+                    }
+                    break;
+                case '2':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_WRITE);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_WRITE);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_WRITE);
+                            break;
+                    }
+                    break;
+                case '3':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_EXECUTE);
+                            perms.add(PosixFilePermission.OWNER_WRITE);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_EXECUTE);
+                            perms.add(PosixFilePermission.GROUP_WRITE);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                            perms.add(PosixFilePermission.OTHERS_WRITE);
+                            break;
+                    }
+                    break;
+                case '4':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_READ);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_READ);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_READ);
+                            break;
+                    }
+                    break;
+                case '5':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_READ);
+                            perms.add(PosixFilePermission.OWNER_EXECUTE);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_READ);
+                            perms.add(PosixFilePermission.GROUP_EXECUTE);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_READ);
+                            perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                            break;
+                    }
+                    break;
+                case '6':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_READ);
+                            perms.add(PosixFilePermission.OWNER_WRITE);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_READ);
+                            perms.add(PosixFilePermission.GROUP_WRITE);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_READ);
+                            perms.add(PosixFilePermission.OTHERS_WRITE);
+                            break;
+                    }
+                    break;
+                case '7':
+                    switch(i){
+                        case 0: 
+                            perms.add(PosixFilePermission.OWNER_READ);
+                            perms.add(PosixFilePermission.OWNER_WRITE);
+                            perms.add(PosixFilePermission.OWNER_EXECUTE);
+                            break;
+                        case 1:
+                            perms.add(PosixFilePermission.GROUP_READ);
+                            perms.add(PosixFilePermission.GROUP_WRITE);
+                            perms.add(PosixFilePermission.GROUP_EXECUTE);
+                            break;
+                        case 2:
+                            perms.add(PosixFilePermission.OTHERS_READ);
+                            perms.add(PosixFilePermission.OTHERS_WRITE);
+                            perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                            break;
+                    }
+                    break;
+            }
+        }
+        Files.setPosixFilePermissions(file.toPath(), perms);
+        
+        /*
+        Set<PosixFilePermission> perms = new HashSet<>();
+        perms.add(PosixFilePermission.OWNER_READ);
+        perms.add(PosixFilePermission.OWNER_WRITE);
+        perms.add(PosixFilePermission.OWNER_EXECUTE);
+
+        perms.add(PosixFilePermission.OTHERS_READ);
+        perms.add(PosixFilePermission.OTHERS_WRITE);
+        perms.add(PosixFilePermission.OTHERS_EXECUTE);
+
+        perms.add(PosixFilePermission.GROUP_READ);
+        perms.add(PosixFilePermission.GROUP_WRITE);
+        perms.add(PosixFilePermission.GROUP_EXECUTE);
+
+        Files.setPosixFilePermissions(file.toPath(), perms);
+        */
+        
+        /*
         window w = new window();
         w.setTitle("Terminal Simulation");
         w.setLocationRelativeTo(null);
         w.setVisible(true);
-        
+        */
     }
-    
 }
