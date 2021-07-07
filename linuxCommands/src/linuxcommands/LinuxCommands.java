440de7c6a69b1a13bd3eb54ed659dca814384d8c
katearzate <katearzate000@gmail.com>
2021-07-07 00:05:04 -0500

    cp command working in GUI

--- linuxCommands/src/linuxcommands/LinuxCommands.java
+++ linuxCommands/src/linuxcommands/LinuxCommands.java
@@ -6,6 +6,12 @@
 package linuxcommands;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 /**
@@ -17,41 +23,8 @@ public class LinuxCommands {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        /*Scanner read = new Scanner(System.in);
-        
-        String command, directory = "";
-        
-        command = read.nextLine(); //command entered
-        
-        String[] datos = command.split(" ");
-
-        switch(datos[0]){
-            case "cp": 
-                System.out.println("solo 1 comando");
-                break;
-            case "ls": 
-                for(String i: datos){
-                    directory = i;
-                }
-                break;
-            default:
-                System.out.println("Comando incorrecto");
-                break;
-        }
-        
-        
-        File directorio = new File(directory); //directorio a listar                                             
-        String[] lista = directorio.list();
-        if(lista == null){
-            System.out.println("\n* * directorio incorrecto * *");
-        }else{
-            for (int i = 0; i < lista.length; i++) {
-                System.out.println(lista[i]);
-            }
-        }
-        */
-        
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Scanner read = new Scanner(System.in);
         
         window w = new window();
         w.setTitle("Terminal Simulation");
--- linuxCommands/src/linuxcommands/window.java
+++ linuxCommands/src/linuxcommands/window.java
@@ -6,6 +6,10 @@
 package linuxcommands;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  *
@@ -48,6 +52,12 @@ public class window extends javax.swing.JFrame {
         jButton2.setText("Delete user");
 
         jTextField1.setText("Enter username...");
+
+        commandText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                commandTextActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -98,37 +108,74 @@ public class window extends javax.swing.JFrame {
         command = commandText.getText();
         String[] datos = command.split(" ");
         
+        //TODO: validacion de que no son más de 3 espacios
+        
         switch(datos[0]){
             case "cp": 
-                listResult.add("1 comando!");
-                break;
-            case "ls": 
-                for(String i: datos){
-                    listResult.clear();
-                    directory = i;
+                listResult.clear();
+                String source = datos[1];
+                String[] parts = source.split("/");
+        
+                int c=-1;
+                for(String i: parts){
+                    c++;
+                }
+                String destination = datos[2]+parts[c]+"-copy";
+                
+                File org = new File(source);
+                File des = new File(destination);
+                
+                try{
+                    InputStream in = new FileInputStream(org);
+                    OutputStream out = new FileOutputStream(des);
+
+                    byte[] buf = new byte[1024];
+                    int len;
+
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                    
+                    listResult.add("Archivo copiado!");
+                }catch(Exception e){
+                    System.out.println("error copiando!");
                 }
                 break;
-            default:
-                System.out.println("Comando incorrecto");
+                
+            case "ls": 
+            //************************* LS COMMAND ********************************
+                listResult.clear();
+                directory = datos[1];
+                
+                File direct = new File(directory); //directorio a listar                                             
+                String[] list = direct.list();
+                if(list == null){
+                    listResult.add("* * Directorio vacío * *");
+                }else{
+                    for (int i = 0; i < list.length; i++) {
+                        listResult.add(list[i]);
+                    }
+                }
+                break;                
+            case "chown":
+                //nomUsuario directorio
                 break;
-        }
-        
-        File direct = new File(directory); //directorio a listar                                             
-        String[] list = direct.list();
-        listResult.clear();
-        if(list == null){
-            listResult.add("* * Directorio vacío * *");
-        }else{
-            for (int i = 0; i < list.length; i++) {
-                listResult.add(list[i]);
-            }
+            case "chmod":
+                //777 directorio
+            default:
+                //chmod | cp | 
+                break;
         }
         
     }//GEN-LAST:event_btnEnterCommandActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+    private void commandTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_commandTextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_commandTextActionPerformed
+
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
